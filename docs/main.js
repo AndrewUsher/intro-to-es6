1b3580504b2782b0926463f420293aa668e29ae9
Andrew Usher <andrewusher00@gmail.com>
2018-12-23 17:23:32 -0600

    Remove gulp and migrate to Parcel

--- docs/main.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function () {
-  let header = document.querySelector('header');
-  let scrollPosition = window.scrollY;
-  if (scrollPosition > 400) {
-    header.classList.add('header-bg');
-  } else {
-    header.classList.remove('header-bg');
-  }
-})
